24945284e458fcc06fd89d0292ea493a41aac64d
Felipe Pinto <felipefernandespinto1093@gmail.com>
2025-10-06 10:50:42 -0300

    feat(renderização doc): Adicionando documentação Swagger como index da aplicação

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask,redirect, Blueprint,url_for
 from config import Config
 from controllers.professor_control import professor_bp
 from controllers.turma_control import turma_bp
@@ -9,8 +9,26 @@ from models.professor import Professor
 from models.turma import Turma
 from models.aluno import Aluno
 from flasgger import Swagger
-
+from flask_swagger_ui import get_swaggerui_blueprint
+SWAGGER_URL = '/swagger'
+API_URL = '/static/doc/doc_swagger.yaml'
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': 'API Monolítica em Flask',
+        'uicolor': '#1a1a1a',
+        'docExpansion': 'list',
+        'tryItOutEnabled': True, 
+        'filter': True,
+        'displayOperationId': True,
+        'requestSnippetsEnabled': True, 
+        'supportedSubmitMethods': ['get', 'post', 'put', 'delete']
+    }
+)
 app= Flask(__name__)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
 app.config.from_object(Config)
 db.init_app(app)
 
@@ -20,6 +38,11 @@ with app.app_context():
 app.register_blueprint(professor_bp)
 app.register_blueprint(turma_bp)
 app.register_blueprint(aluno_bp)
+
+@app.route('/')
+def index():
+    return redirect('/swagger')
+
 
 if __name__ == '__main__':
     app.run(debug=True) 
